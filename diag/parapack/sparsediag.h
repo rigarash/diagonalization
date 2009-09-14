c30eff909fac8611a41c774788134c382c95d1ce
Ryo IGARASHI <rigarash@gmail.com>
2009-09-14 12:40:21 +0900

    No need to split.

--- diag/parapack/sparsediag.h
+++ diag/parapack/sparsediag.h
@@ -62,73 +62,68 @@ class sparsediag_worker
     sparsediag_worker(alps::Parameters const& params)
         : super_type(params)
     {}
-    void run(alps::ObservableSet& /* obs */);
+    void run(alps::ObservableSet& obs) {
+        typedef typename boost::numeric::ublas::vector<double> vector_type;
 
+        if (this->progress() >= 1.0) { return; }
+        this->is_diagonalized_ = true;
+
+        // measurements
+        std::map<std::string, double> m;
+        m["Number of Sites"] = this->num_sites();
+        m["Volume"] = this->volume();
+
+        // Lanczos diagonalization of Hamiltonian matrix
+        vector_type evals;
+        {
+            typedef ietl::vectorspace<vector_type> vectorspace_type;
+            boost::mt19937 generator;
+            vectorspace_type vec(this->dimension());
+#ifdef ALPS_HAVE_MKL
+            typedef typename boost::numeric::ublas::compressed_matrix<double, boost::numeric::ublas::row_major> matrix2_type;
+            matrix2_type Hamiltonian2 = this->matrix();
+            ietl::lanczos<matrix2_type, vectorspace_type> lanczos(Hamiltonian2, vec);
+#else
+            ietl::lanczos<matrix_type, vectorspace_type> lanczos(matrix(), vec);
+#endif
+            int max_iter = std::min(static_cast<int>(10 * this->dimension()), 1000);
+            if (this->params_.defined("MAX_ITERATIONS")) {
+                max_iter = this->params_["MAX_ITERATIONS"];
+            }
+            std::size_t num_eigenvalues = 1;
+            if (this->params_.defined("NUMBER_EIGENVALUES")) {
+                num_eigenvalues = this->params_["NUMBER_EIGENVALUES"];
+            }
+            ietl::lanczos_iteration_nlowest<double> iter(max_iter, num_eigenvalues);
+            boost::timer t;
+            std::clog << "Starting Lanczos... " << std::flush;
+            lanczos.calculate_eigenvalues(iter, generator);
+            std::clog << "done. Elapsed time: " << t.elapsed() << std::endl;
+
+            int n = std::min(num_eigenvalues, lanczos.eigenvalues().size());
+            evals.resize(n);
+            for (std::size_t i = 0; i < n; ++i) {
+                evals[i] = lanczos.eigenvalues()[i];
+            }
+        }
+
+        double E0 = evals[0];
+
+        m["Ground State Energy"] = E0;
+        m["Ground State Energy Density" ] = E0 / this->volume();
+
+        // Store measurements
+        typedef std::pair<std::string, double> pair_type;
+        BOOST_FOREACH(pair_type const& p, m) {
+            obs << alps::SimpleRealObservable(p.first);
+        }
+        obs.reset(true);
+        BOOST_FOREACH(pair_type const& p, m) {
+            obs[p.first] << p.second;
+        }
+    }
 };
 
-template <typename T, typename M1>
-void
-sparsediag_worker<T, M1>::run(alps::ObservableSet& obs)
-{
-    typedef typename boost::numeric::ublas::vector<double> vector_type;
-
-    if (this->progress() >= 1.0) { return; }
-    this->is_diagonalized_ = true;
-
-    // measurements
-    std::map<std::string, double> m;
-    m["Number of Sites"] = this->num_sites();
-    m["Volume"] = this->volume();
-
-    // Lanczos diagonalization of Hamiltonian matrix
-    vector_type evals;
-    {
-        typedef ietl::vectorspace<vector_type> vectorspace_type;
-        boost::mt19937 generator;
-        vectorspace_type vec(this->dimension());
-#ifdef ALPS_HAVE_MKL
-        typedef typename boost::numeric::ublas::compressed_matrix<double, boost::numeric::ublas::row_major> matrix2_type;
-        matrix2_type Hamiltonian2 = this->matrix();
-        ietl::lanczos<matrix2_type, vectorspace_type> lanczos(Hamiltonian2, vec);
-#else
-        ietl::lanczos<matrix_type, vectorspace_type> lanczos(matrix(), vec);
-#endif
-        int max_iter = std::min(static_cast<int>(10 * this->dimension()), 1000);
-        if (this->params_.defined("MAX_ITERATIONS")) {
-            max_iter = this->params_["MAX_ITERATIONS"];
-        }
-        std::size_t num_eigenvalues = 1;
-        if (this->params_.defined("NUMBER_EIGENVALUES")) {
-            num_eigenvalues = this->params_["NUMBER_EIGENVALUES"];
-        }
-        ietl::lanczos_iteration_nlowest<double> iter(max_iter, num_eigenvalues);
-        boost::timer t;
-        std::clog << "Starting Lanczos... " << std::flush;
-        lanczos.calculate_eigenvalues(iter, generator);
-        std::clog << "done. Elapsed time: " << t.elapsed() << std::endl;
-
-        int n = std::min(num_eigenvalues, lanczos.eigenvalues().size());
-        evals.resize(n);
-        for (std::size_t i = 0; i < n; ++i) {
-            evals[i] = lanczos.eigenvalues()[i];
-        }
-    }
-
-    double E0 = evals[0];
-
-    m["Ground State Energy"] = E0;
-    m["Ground State Energy Density" ] = E0 / this->volume();
-
-    // Store measurements
-    typedef std::pair<std::string, double> pair_type;
-    BOOST_FOREACH(pair_type const& p, m) {
-        obs << alps::SimpleRealObservable(p.first);
-    }
-    obs.reset(true);
-    BOOST_FOREACH(pair_type const& p, m) {
-        obs[p.first] << p.second;
-    }
-}
 
 } // end namespace diag
 } // end namespace alps
