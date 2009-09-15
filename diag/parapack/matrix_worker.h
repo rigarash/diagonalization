af7aad0ac3aba8105e142b09267937f67d9e7703
Ryo IGARASHI <rigarash@gmail.com>
2009-09-15 12:26:01 +0900

    Add more copyright info.

--- diag/parapack/matrix_worker.h
+++ diag/parapack/matrix_worker.h
@@ -3,7 +3,9 @@
 * ALPS/diag/parapack: Full and Sparse diagonalization
 *                     for quantum lattice models using parapack scheduler
 *
-* Copyright (C) 2009-2009 by Ryo IGARASHI <rigarash@hosi.phys.s.u-tokyo.ac.jp>
+* Copyright (C) 1994-2006 by Matthias Troyer <troyer@comp-phys.org>,
+*               1994-2006 by Andreas Honecker <ahoneck@uni-goettingen.de>
+*               2009-2009 by Ryo IGARASHI <rigarash@hosi.phys.s.u-tokyo.ac.jp>
 *
 * This software is published under the ALPS Application License; you
 * can use, redistribute it and/or modify it under the terms of the
