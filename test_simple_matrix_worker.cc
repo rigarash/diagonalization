26cb80299741f49f4151d53f5b2f2ab30128efbd
Ryo IGARASHI <rigarash@gmail.com>
2013-01-07 12:12:54 +0900

    Properly show that the worker changed during save and load.

--- test_simple_matrix_worker.cc
+++ test_simple_matrix_worker.cc
@@ -46,39 +46,38 @@ BOOST_AUTO_TEST_CASE(simple_matrix_worker_h) {
     boost::filesystem::path dump(boost::filesystem::unique_path());
 
     // start test
-    alps::diag::simple_matrix_worker<> w1(p);
-    w1.init_observables(p, obs);
-
-    // precondition check
-    // always thermalized
-    BOOST_CHECK(w1.is_thermalized());
-    // always finished
-    BOOST_CHECK_GE(w1.progress(), 1.0);
-
-    // check run
-    w1.run(obs);
-
-    // check save
     {
+        alps::diag::simple_matrix_worker<> w1(p);
+        w1.init_observables(p, obs);
+
+        // precondition check
+        // always thermalized
+        BOOST_CHECK(w1.is_thermalized());
+        // always finished
+        BOOST_CHECK_GE(w1.progress(), 1.0);
+
+        // check run
+        w1.run(obs);
+
+        // check save
         alps::OXDRFileDump odp(dump);
         w1.save(odp);
     }
 
     // restart test
-    alps::diag::simple_matrix_worker<> w2(p);
     {
+        alps::diag::simple_matrix_worker<> w2(p);
         alps::IXDRFileDump idp(dump);
         w2.load(idp);
-    }
 
-    // postcondition check
-    // always thermalized
-    BOOST_CHECK(w2.is_thermalized());
-    // finishied
-    BOOST_CHECK_GE(w2.progress(), 1.0);
+        // postcondition check
+        // always thermalized
+        BOOST_CHECK(w2.is_thermalized());
+        // always finished
+        BOOST_CHECK_GE(w2.progress(), 1.0);
+    }
 
     // post execution cleanup
     // remove temporary dump files
-    bool status = boost::filesystem::remove(dump);
-    BOOST_CHECK(status);
+    BOOST_CHECK(boost::filesystem::remove(dump));
 }
